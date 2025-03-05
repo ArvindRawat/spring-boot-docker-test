7272b2e65fc2ba5b676ed12794034c113fbb4791
Arvind Rawat <er.arvindrawat@hotmail.com>
2025-03-05 18:44:53 +0530

    commit basic code

---
+++ src/test/java/com/arvind/SpringBootApplicatioTest/SpringBootApplicatioTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arvind.SpringBootApplicatioTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApplicatioTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
